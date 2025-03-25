398ca2684536c2b47de5baf03e6a69992afca959
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-25 10:50:21 +0100

    Create project

---
+++ AudioScrubber/AudioScrubber/AudioScrubberApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioScrubberApp.swift
+//  AudioScrubber
+//
+//  Created by Jesus Antonio Gil on 25/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioScrubberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
